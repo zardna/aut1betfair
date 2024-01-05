2a813ead175722acbf7673ae33237d65e8cba2b1
badescua <andrada.badescu@ppb.com>
2024-01-05 17:53:23 +0200

    HW

--- src/main/java/homeworks/LeapYear.java
+++ src/main/java/homeworks/LeapYear.java
@@ -8,20 +8,44 @@ package homeworks;
 
 
 public class LeapYear {
-    public static void main(String[] args) {
 
-        for (int i = 1900; i <= 2016; i++) {
-            if (i % 4 == 0) {
-                if (i % 100 != 0 || i % 400 == 0) {
-                    System.out.println(i + ": A leap Year");
-                } else {
-                    System.out.println(i + ": Not a leap year");
-                }
-            } else {
-                System.out.println(i + ": Not a leap year");
-            }
+    public static void main(String args[]) {
+
+        int year = 1999;
+
+        if (year % 4 != 0 && (year % 100 == 0 || year % 400 != 0)) {
+            System.out.println("In february " + year + " there are 28 days.");
+        } else {
+            System.out.println("In february " + year + " there are 29 days.");
         }
     }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+//public class LeapYear {
+//    public static void main(String[] args) {
+//
+//        for (int i = 1900; i <= 2016; i++) {
+//            if (i % 4 == 0) {
+//                if (i % 100 != 0 || i % 400 == 0) {
+//                    System.out.println(i + ": A leap Year");
+//                } else {
+//                    System.out.println(i + ": Not a leap year");
+//                }
+//            } else {
+//                System.out.println(i + ": Not a leap year");
+//            }
+//        }
+//    }
+//}
