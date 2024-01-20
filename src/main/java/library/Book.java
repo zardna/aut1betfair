e71c3ce4b8d59f53e5e0b98d866b2147ba212c01
badescua <andrada.badescu@ppb.com>
2024-01-20 17:33:55 +0200

    HW

--- src/main/java/library/Book.java
+++ src/main/java/library/Book.java
@@ -2,41 +2,32 @@ package library;
 
 
 public class Book {
-    @Override
-    public String toString() {
-        return "Book{" +
-                "name='" + name + '\'' +
-                ", author=" + author +
-                ", price=" + price +
-                ", year=" + year +
-                '}';
-    }
 
     private String name;
-    private String author;
+    private Author author;
     private double price;
     private int year;
 
-    public Book(String name, String author, double price, int year) {
+
+    public Book(String name, Author author, double price, int year) {
         this.name = name;
         this.author = author;
         this.price = price;
         this.year = year;
     }
 
-
     public String getName() {
         return name;
     }
-    public String getAuthor() {
+
+    public Author getAuthor() {
         return author;
     }
+
     public double getPrice() {
         return price;
     }
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+
     public int getYear() {
         return year;
     }
--- src/main/java/library/Library.java
+++ src/main/java/library/Library.java
@@ -1,6 +1,10 @@
 package library;
 
 public class Library {
-    private String author;
-    private String book;
+    public static void main(String[] args) {
+        Author backman = new Author("Backman", "fbackman@ppb.com");
+        Book invingatorii = new Book("Invingatorii",backman,50.23,2023);
+        System.out.println("Book "+invingatorii.getName()+" ("+ invingatorii.getPrice()+" RON),"+" by "+ invingatorii.getAuthor().getName()+", published in "+ invingatorii.getYear());
+
+    }
 }
